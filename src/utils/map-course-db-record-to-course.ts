f5dbd44731745ec003ee8ad802826ca361e0239f
R0Mkka <alkotel.1999@mail.ru>
2021-10-10 16:38:49 +0200

    Introduce mapCourseDbRecordToCourse util function

---
+++ src/utils/map-course-db-record-to-course.ts
@@ -0,0 +1,14 @@
+import { Course } from '../types/course';
+import { CourseDbRecord } from '../types/course-db-record';
+
+export const mapCourseDbRecordToCourse = (
+  dbRecord: CourseDbRecord,
+): Course => ({
+  id: dbRecord.id,
+  title: dbRecord.title,
+  description: dbRecord.description,
+  wantToImprove: dbRecord.want_to_improve,
+  lectures: [],
+  createdAt: dbRecord.created_at,
+  modifiedAt: dbRecord.modified_at,
+});
