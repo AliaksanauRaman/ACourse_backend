65ccca89bd65460792d982225a1a977d67d499bc
R0Mkka <alkotel.1999@mail.ru>
2021-10-10 12:52:43 +0200

    Add getAll, getById, create and delete handlers for Courses API

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,14 +1,37 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+
+import { CoursesService } from './courses.service';
+
+import { Course } from '../types/course';
+import { CreateCourseDto } from '../types/create-course-dto';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
   @Get('/')
-  getAllCourses(): Array<any> {
-    return [];
+  async handleGetAllCourses(): Promise<Array<Course>> {
+    return this.coursesService.getAllCourses();
   }
 
   @Get('/:courseId')
-  getCourseById(@Param('courseId') courseId: string): string {
-    return courseId;
+  async handleGetCourseById(
+    @Param('courseId') courseId: string,
+  ): Promise<Course> {
+    return this.coursesService.getCourseById(courseId);
+  }
+
+  @Post('/')
+  async handleCreateCourse(
+    @Body() createCourseDto: CreateCourseDto,
+  ): Promise<Course> {
+    return this.coursesService.createCourse(createCourseDto);
+  }
+
+  @Delete('/:courseId')
+  async handleDeleteCourse(
+    @Param('courseId') courseId: string,
+  ): Promise<Course> {
+    return this.coursesService.deleteCourse(courseId);
   }
 }
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,8 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Pool } from 'pg';
 
+import { PG_POOL } from '../db/pg-pool';
+import { Course } from '../types/course';
+import { CreateCourseDto } from '../types/create-course-dto';
+
+const COURSES_TABLE_NAME = 'Courses';
 @Injectable()
 export class CoursesService {
-  getAllCourses(): Array<any> {
-    return [];
+  constructor(@Inject(PG_POOL) private readonly dbPool: Pool) {}
+
+  async getAllCourses(): Promise<Array<Course>> {
+    return this.dbPool
+      .query<Course>(
+        `
+        SELECT *
+        FROM "${COURSES_TABLE_NAME}";
+      `,
+      )
+      .then(({ rows }) => rows);
+  }
+
+  async getCourseById(courseId: string): Promise<Course> {
+    return this.dbPool
+      .query<Course>(
+        `
+        SELECT *
+        FROM "${COURSES_TABLE_NAME}"
+        WHERE id=$1
+        FETCH FIRST ROW ONLY
+      `,
+        [courseId],
+      )
+      .then(({ rows }) => {
+        const firstRow = rows[0];
+
+        if (firstRow === undefined) {
+          throw new NotFoundException('Course with provided id was not found!');
+        }
+
+        return firstRow;
+      });
+  }
+
+  async createCourse(createCourseDto: CreateCourseDto): Promise<Course> {
+    return this.dbPool
+      .query<Course>(
+        `
+        INSERT INTO "${COURSES_TABLE_NAME}" (title, description, want_to_improve)
+        VALUES ($1, $2, $3)
+        RETURNING *;
+      `,
+        [
+          createCourseDto.title,
+          createCourseDto.description,
+          createCourseDto.wantToImprove,
+        ],
+      )
+      .then(({ rows }) => ({
+        ...rows[0],
+        lectures: [], // TODO
+      }));
+  }
+
+  async deleteCourse(courseId: string): Promise<Course> {
+    return this.dbPool
+      .query<Course>(
+        `
+          DELETE FROM "${COURSES_TABLE_NAME}"
+          WHERE id=$1
+          RETURNING *;
+        `,
+        [courseId],
+      )
+      .then(({ rowCount, rows }) => {
+        if (rowCount === 1) {
+          return {
+            ...rows[0],
+            lectures: [], // TODO
+          };
+        }
+
+        throw new NotFoundException('Course with provided id was not found!');
+      });
   }
 }
