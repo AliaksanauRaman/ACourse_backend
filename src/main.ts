6897bc56572a2769de3f03bc9774f5773cb28145
R0Mkka <alkotel.1999@mail.ru>
2021-10-10 17:28:53 +0200

    Add CORS with no config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(); // TODO: Config cors
+
   await app.listen(process.env.PORT || 3333);
 }
 
