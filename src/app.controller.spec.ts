5971ba0e84ee6a2f973b3cb8a254fc4b63b463fc
R0Mkka <alkotel.1999@mail.ru>
2021-10-10 13:03:31 +0200

    Adjust the return value of root url

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return correct welcome message', () => {
+      expect(appController.handleGetWelcomeMessage()).toBe(
+        'Welcome to ACourse!',
+      );
     });
   });
 });
