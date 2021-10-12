8ddae423b6ec36ad90ed7bb381fe1522b77f767f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-10-12 09:58:33 +0200

    Add isLectureDbRecord type assertion function

--- src/courses/types/lecture-db-record.ts
+++ src/courses/types/lecture-db-record.ts
@@ -1,8 +1,21 @@
+import { isDate } from '../../shared/utils/is-date';
+import { isObject } from '../../shared/utils/is-object';
+import { isString } from '../../shared/utils/is-string';
+
 export type LectureDbRecord = {
   readonly id: string;
   readonly course_id: string;
   readonly title: string;
   readonly description: string;
-  readonly created_at: Date; // TODO: Check
-  readonly modified_at: Date; // TODO: Check
+  readonly created_at: Date;
+  readonly modified_at: Date;
 };
+
+export const isLectureDbRecord = (value: unknown): value is LectureDbRecord =>
+  isObject(value) &&
+  isString(value.id) &&
+  isString(value.course_id) &&
+  isString(value.title) &&
+  isString(value.description) &&
+  isDate(value.created_at) &&
+  isDate(value.modified_at);
