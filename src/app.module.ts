127d52ef1f28e880e58f11623cc9f5310b23d7f5
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-10-12 20:45:23 +0200

    Configure S3Module, add it to the AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { S3Module } from 'nestjs-s3';
 
 import { CoursesModule } from './courses/courses.module';
 
@@ -7,7 +9,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), CoursesModule],
+  imports: [
+    ConfigModule.forRoot({}),
+    S3Module.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        config: {
+          accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
+          region: 'us-east-1',
+          s3ForcePathStyle: true,
+          signatureVersion: 'v4',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    CoursesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
