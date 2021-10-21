905567e57d3b8e313a1bc1acd6e06a906870eb8b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-10-21 13:20:58 +0200

    Add lectureDbRecordFactory

---
+++ src/testing/tools/factories/lecture-db-record-factory.ts
@@ -0,0 +1,12 @@
+import { Factory } from 'fishery';
+import { v4 as uuid } from 'uuid';
+import { LectureDbRecord } from '../../../courses/types/lecture-db-record';
+
+export const lectureDbRecordFactory = Factory.define<LectureDbRecord>(() => ({
+  id: uuid(),
+  course_id: uuid(),
+  title: 'Lecture title',
+  description: 'Lecture description',
+  created_at: new Date(),
+  modified_at: new Date(),
+}));
