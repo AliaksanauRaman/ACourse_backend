799bfca685a62dd34ac2c912235171b7761ff010
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-10-21 13:20:40 +0200

    Add isCourse type assertion function

--- src/courses/types/course.ts
+++ src/courses/types/course.ts
@@ -1,3 +1,8 @@
+import { isObject } from '../../shared/utils/is-object';
+import { isString } from '../../shared/utils/is-string';
+import { isDate } from '../../shared/utils/is-date';
+import { isBoolean } from '../../shared/utils/is-boolean';
+
 export type Course = {
   readonly id: string;
   readonly title: string;
@@ -6,3 +11,12 @@ export type Course = {
   readonly createdAt: Date;
   readonly modifiedAt: Date;
 };
+
+export const isCourse = (value: unknown): value is Course =>
+  isObject(value) &&
+  isString(value.id) &&
+  isString(value.title) &&
+  isString(value.description) &&
+  isBoolean(value.wantToImprove) &&
+  isDate(value.createdAt) &&
+  isDate(value.modifiedAt);
