ee901e283ac3e97a560c2021cb960d73657977fc
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-10-27 20:14:54 +0200

    Copy Course type and its type assertion function

---
+++ src/api/courses/types/course.type.ts
@@ -0,0 +1,22 @@
+import { isObject } from '../../../shared/utils/is-object.util';
+import { isString } from '../../../shared/utils/is-string.util';
+import { isDate } from '../../../shared/utils/is-date.util';
+import { isBoolean } from '../../../shared/utils/is-boolean.util';
+
+export type Course = {
+  readonly id: string;
+  readonly title: string;
+  readonly description: string;
+  readonly wantToImprove: boolean;
+  readonly createdAt: Date;
+  readonly modifiedAt: Date;
+};
+
+export const isCourse = (value: unknown): value is Course =>
+  isObject(value) &&
+  isString(value.id) &&
+  isString(value.title) &&
+  isString(value.description) &&
+  isBoolean(value.wantToImprove) &&
+  isDate(value.createdAt) &&
+  isDate(value.modifiedAt);
