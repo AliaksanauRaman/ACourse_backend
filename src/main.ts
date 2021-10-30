ad1a0765f6331450c71d70ab5a8c84b4430c47b2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-10-30 13:24:59 +0200

    Make a use of setupSwagger function

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { CORS_OPTIONS } from './core/cors-options';
+import { setupSwagger } from './core/setup-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors(CORS_OPTIONS);
   app.useGlobalPipes(new ValidationPipe());
+
+  setupSwagger(app);
 
   await app.listen(process.env.PORT || 3333);
 }
