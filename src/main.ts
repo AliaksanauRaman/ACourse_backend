f0b711acda439bc4a91f812e51ec9b21ad972e92
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-10-30 13:30:41 +0200

    Make a use of setupCors function, delete the file with cors options

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,14 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-import { CORS_OPTIONS } from './core/cors-options';
+import { setupCors } from './core/setup-cors';
 import { setupSwagger } from './core/setup-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(CORS_OPTIONS);
+  setupCors(app);
   app.useGlobalPipes(new ValidationPipe());
-
   setupSwagger(app);
 
   await app.listen(process.env.PORT || 3333);
