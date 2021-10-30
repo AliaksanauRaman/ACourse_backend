b103279fd74fa09d2582254e50244b403ad01a16
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-10-30 13:30:06 +0200

    Add setupCors function

---
+++ src/core/setup-cors.ts
@@ -0,0 +1,10 @@
+import { INestApplication } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const CORS_OPTIONS: CorsOptions = {
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+};
+
+export const setupCors = (app: INestApplication): void =>
+  app.enableCors(CORS_OPTIONS);
