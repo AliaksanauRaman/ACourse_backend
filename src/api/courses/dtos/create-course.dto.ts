e1d38d32d4c2624f7b84b904b426fac5af37f5aa
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-11-01 10:29:16 +0100

    Add swagger decorators to CreateCourseDto class fields

--- src/api/courses/dtos/create-course.dto.ts
+++ src/api/courses/dtos/create-course.dto.ts
@@ -1,20 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCourseDto {
+  @ApiProperty({ minLength: 1, maxLength: 60 })
   @IsString()
   @IsNotEmpty()
   readonly title: string;
 
+  // TODO: Make max field length to 255 in sql
+  @ApiProperty({ minLength: 1, maxLength: 255 })
   @IsString()
   @IsNotEmpty()
   readonly description: string;
 
+  @ApiProperty()
   @IsBoolean()
   readonly wantToImprove: boolean;
-
-  constructor(title: string, description: string, wantToImprove: boolean) {
-    this.title = title;
-    this.description = description;
-    this.wantToImprove = wantToImprove;
-  }
 }
