515e93a981d35a0dfb31020fc0319f54224d30e9
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-12-02 10:19:49 +0100

    Adjust CreateLessonDto type so that it becomes processable for swagger

--- src/api/lessons/dtos/create-lesson.dto.ts
+++ src/api/lessons/dtos/create-lesson.dto.ts
@@ -1,30 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { LessonType } from '../types/lesson-type.type';
 
 export class CreateLessonDto {
+  @ApiProperty({ type: String, minLength: 36, maxLength: 36 })
   @IsUUID('4')
   readonly courseId: string;
 
+  @ApiProperty({ minLength: 1, maxLength: 60 })
   @IsString()
   @IsNotEmpty()
   readonly title: string;
 
+  @ApiProperty({ enum: LessonType })
   @IsEnum(LessonType)
   readonly type: LessonType;
 
+  // TODO: Make max field length to 255 in sql
+  @ApiProperty({ minLength: 1, maxLength: 255 })
   @IsString()
   @IsNotEmpty()
   readonly description: string;
-
-  constructor(
-    courseId: string,
-    title: string,
-    type: LessonType,
-    description: string,
-  ) {
-    this.courseId = courseId;
-    this.title = title;
-    this.type = type;
-    this.description = description;
-  }
 }
