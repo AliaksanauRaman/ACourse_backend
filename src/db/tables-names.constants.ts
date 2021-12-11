d896c0db16447a6103472f2e6b484a91ba8255fe
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2021-12-11 08:35:00 +0100

    Add new tables names to the db/tables-names.constants.ts file, also leave a todo comment in order to think about replacing these bunch of constants with single enum

--- src/db/tables-names.constants.ts
+++ src/db/tables-names.constants.ts
@@ -1,2 +1,6 @@
+// TODO: Think about enum?
 export const COURSES_TABLE_NAME = 'courses';
 export const LESSONS_TABLE_NAME = 'lessons';
+export const TASKS_TABLE_NAME = 'tasks';
+export const LESSONS_FILES_TABLE_NAME = 'lessons_files';
+export const TASKS_FILES_TABLE_NAME = 'tasks_files';
