95996b4d3e2dc754ac03b5dc1293e73dd191a7d7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-09 16:57:50 +0100

    Add DbTableName enum with USERS_COURSES table name

--- src/modules/db/tables-names.constants.ts
+++ src/modules/db/tables-names.constants.ts
@@ -5,3 +5,7 @@ export const TASKS_TABLE_NAME = 'tasks';
 export const LESSONS_FILES_TABLE_NAME = 'lessons_files';
 export const TASKS_FILES_TABLE_NAME = 'tasks_files';
 export const USERS_TABLE_NAME = 'users';
+
+export enum DbTableName {
+  USERS_COURSES = 'users_courses',
+}
