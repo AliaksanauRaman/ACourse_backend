401382714daf2a7d61cc6d37af46fe1773c96ce7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-11 16:37:10 +0100

    Add swagger to AuthenticationController handleUserLoginByCredentials method

--- src/modules/authentication/authentication.controller.ts
+++ src/modules/authentication/authentication.controller.ts
@@ -1,13 +1,24 @@
 import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+import { Endpoint } from '../../constants/endpoints';
 import { UserWithoutPassword } from '../users/types/user-without-password.type';
 import { User } from '../../shared/decorators/user.decorator';
 
 import { AuthenticationService } from './authentication.service';
 import { CredentialsAuthenticationGuard } from './guards/credentials-authentication.guard';
 import { JwtAuthenticationGuard } from './guards/jwt-authentication.guard';
+import { UserCredentialsDto } from './dtos/user-credentials.dto';
+import { UserLoginResponse } from './types/user-login-response.type';
+import { UnauthorizedError } from './types/unauthorized-error.type';
 
-@Controller('api/auth')
+@ApiTags(Endpoint.AUTHENTICATION)
+@Controller(`api/${Endpoint.AUTHENTICATION}`)
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
@@ -19,11 +30,14 @@ export class AuthenticationController {
     return user;
   }
 
+  @ApiBody({ type: UserCredentialsDto })
+  @ApiCreatedResponse({ type: UserLoginResponse })
+  @ApiUnauthorizedResponse({ type: UnauthorizedError })
   @UseGuards(CredentialsAuthenticationGuard)
   @Post('/login')
-  async handleLogin(
+  async handleUserLoginByCredentials(
     @User() user: UserWithoutPassword,
-  ): Promise<{ accessToken: string }> {
+  ): Promise<UserLoginResponse> {
     const accessToken =
       await this.authenticationService.generateJwtByUserPayload(user);
     return { accessToken };
