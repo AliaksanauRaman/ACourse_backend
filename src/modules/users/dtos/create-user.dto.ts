66883e19be8e8f04eb825822ba47618471f32468
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-11 15:48:41 +0100

    Enhance CreateUserDto type

--- src/modules/users/dtos/create-user.dto.ts
+++ src/modules/users/dtos/create-user.dto.ts
@@ -1,27 +1,43 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+import { UserInputLimitation } from '../../../constants/user-input-limitation';
 
 export class CreateUserDto {
-  @ApiProperty({ minLength: 1, maxLength: 50 })
+  @ApiProperty({
+    minLength: 1,
+    maxLength: UserInputLimitation.FIRST_NAME_MAX_LENGTH,
+  })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(UserInputLimitation.FIRST_NAME_MAX_LENGTH)
   readonly first_name: string;
 
-  @ApiProperty({ minLength: 1, maxLength: 50 })
+  @ApiProperty({
+    minLength: 1,
+    maxLength: UserInputLimitation.LAST_NAME_MAX_LENGTH,
+  })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(UserInputLimitation.LAST_NAME_MAX_LENGTH)
   readonly last_name: string;
 
-  @ApiProperty({ minLength: 1, maxLength: 70 })
+  @ApiProperty({
+    minLength: 1,
+    maxLength: UserInputLimitation.EMAIL_MAX_LENGTH,
+  })
   @IsString()
   @IsNotEmpty()
   @IsEmail()
+  @MaxLength(UserInputLimitation.EMAIL_MAX_LENGTH)
   readonly email: string;
 
-  // maxLength here represents the maximum length of a password
-  // from user, but not the actual maxLength of a password in the db
-  @ApiProperty({ minLength: 1, maxLength: 50 })
+  @ApiProperty({
+    minLength: 1,
+    maxLength: UserInputLimitation.PASSWORD_MAX_LENGTH,
+  })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(UserInputLimitation.PASSWORD_MAX_LENGTH)
   readonly password: string;
 }
